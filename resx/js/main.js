3f13805996e2c7f50c3dcc5547682bd20776d1d1
knotuteye <kevinotuteye@gmail.com>
2020-04-28 18:44:55 +0000

    added svgs

--- resx/js/main.js
+++ resx/js/main.js
@@ -1,54 +1,41 @@
-const init = () => {
-	setTimeout(function () {
-		let viewheight = $(window).height()
-		let viewwidth = $(window).width()
-		let viewport = document.querySelector('meta[name=viewport]')
-		viewport.setAttribute(
-			'content',
-			'height=' +
-				viewheight +
-				'px, width=' +
-				viewwidth +
-				'px, initial-scale=1.0'
-		)
-	}, 300)
-	fetch(
-		'https://prime.exchangerate-api.com/v5/4fff55994a5ddad27798dd80/latest/USD'
-	)
-		.then((response) => response.json())
-		.then((data) => {
-			for (
-				let i = 0;
-				i < document.getElementsByTagName('select').length;
-				i++
-			) {
-				for (const currency in data.conversion_rates) {
-					let opt = document.createElement('option')
-					opt.text = currency
-					opt.value = data.conversion_rates[currency]
-					document.getElementsByTagName('select')[i].appendChild(opt)
-				}
-			}
-			let refreshTime = new Date(data.time_last_update * 1000)
-			document.getElementById(
-				'update-time'
-			).innerText += ` ${refreshTime.getDate()}\\${refreshTime.getMonth()}\\${refreshTime.getFullYear()}`
-		})
-		.then(() => {
-			for (
-				let i = 0;
-				i < document.getElementsByTagName('select').length;
-				i++
-			) {
-				document.getElementsByTagName('select')[
-					i
-				].value = document.getElementsByTagName('select')[i].children[
-					i + 1
-				].value
-			}
-		})
-		.catch((err) => alert(err))
-}
+// const init = () => {
+// 	fetch(
+// 		'https://prime.exchangerate-api.com/v5/4fff55994a5ddad27798dd80/latest/USD'
+// 	)
+// 		.then((response) => response.json())
+// 		.then((data) => {
+// 			for (
+// 				let i = 0;
+// 				i < document.getElementsByTagName('select').length;
+// 				i++
+// 			) {
+// 				for (const currency in data.conversion_rates) {
+// 					let opt = document.createElement('option')
+// 					opt.text = currency
+// 					opt.value = data.conversion_rates[currency]
+// 					document.getElementsByTagName('select')[i].appendChild(opt)
+// 				}
+// 			}
+// 			let refreshTime = new Date(data.time_last_update * 1000)
+// 			document.getElementById(
+// 				'update-time'
+// 			).innerText += ` ${refreshTime.getDate()}\\${refreshTime.getMonth()}\\${refreshTime.getFullYear()}`
+// 		})
+// 		.then(() => {
+// 			for (
+// 				let i = 0;
+// 				i < document.getElementsByTagName('select').length;
+// 				i++
+// 			) {
+// 				document.getElementsByTagName('select')[
+// 					i
+// 				].value = document.getElementsByTagName('select')[i].children[
+// 					i + 1
+// 				].value
+// 			}
+// 		})
+// 		.catch((err) => alert(err))
+// }
 
 /**
  *
