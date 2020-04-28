fda01ba32e2955fbb1bdc8d393b9b51501181f02
knotuteye <kevinotuteye@gmail.com>
2020-04-28 19:21:25 +0000

    refined logic and fixed css issues

--- resx/js/main.js
+++ resx/js/main.js
@@ -1,41 +1,50 @@
-// const init = () => {
-// 	fetch(
-// 		'https://prime.exchangerate-api.com/v5/4fff55994a5ddad27798dd80/latest/USD'
-// 	)
-// 		.then((response) => response.json())
-// 		.then((data) => {
-// 			for (
-// 				let i = 0;
-// 				i < document.getElementsByTagName('select').length;
-// 				i++
-// 			) {
-// 				for (const currency in data.conversion_rates) {
-// 					let opt = document.createElement('option')
-// 					opt.text = currency
-// 					opt.value = data.conversion_rates[currency]
-// 					document.getElementsByTagName('select')[i].appendChild(opt)
-// 				}
-// 			}
-// 			let refreshTime = new Date(data.time_last_update * 1000)
-// 			document.getElementById(
-// 				'update-time'
-// 			).innerText += ` ${refreshTime.getDate()}\\${refreshTime.getMonth()}\\${refreshTime.getFullYear()}`
-// 		})
-// 		.then(() => {
-// 			for (
-// 				let i = 0;
-// 				i < document.getElementsByTagName('select').length;
-// 				i++
-// 			) {
-// 				document.getElementsByTagName('select')[
-// 					i
-// 				].value = document.getElementsByTagName('select')[i].children[
-// 					i + 1
-// 				].value
-// 			}
-// 		})
-// 		.catch((err) => alert(err))
-// }
+const socialLinks = [
+	'https://github.com/knotuteye',
+	'mailto:kevinotuteye@gmail.com',
+	'https://t.me/otuteye',
+]
+
+const init = () => {
+	fetch(
+		'https://prime.exchangerate-api.com/v5/4fff55994a5ddad27798dd80/latest/USD'
+	)
+		.then((response) => response.json())
+		.then((data) => {
+			for (
+				let i = 0;
+				i < document.getElementsByTagName('select').length;
+				i++
+			) {
+				for (const currency in data.conversion_rates) {
+					let opt = document.createElement('option')
+					opt.text = currency
+					opt.value = data.conversion_rates[currency]
+					document.getElementsByTagName('select')[i].appendChild(opt)
+				}
+			}
+			let refreshTime = new Date(data.time_last_update * 1000)
+			document.getElementById(
+				'update-time'
+			).innerText += ` ${refreshTime.getDate()}\\${refreshTime.getMonth()}\\${refreshTime.getFullYear()}`
+		})
+		.then(() => {
+			document.getElementsByTagName(
+				'select'
+			)[1].value = document.getElementsByTagName(
+				'select'
+			)[1].children[1].value
+		})
+		.then(() => {
+			for (let i = 0; i < socialLinks.length; i++) {
+				document
+					.getElementsByTagName('svg')
+					[i].addEventListener('click', (ev) => {
+						window.open(socialLinks[i])
+					})
+			}
+		})
+		.catch((err) => alert(err))
+}
 
 /**
  *
