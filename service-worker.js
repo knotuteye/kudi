51fac99f2ce4dd8397142ea96014a9afb723a778
knotuteye <kevinotuteye@gmail.com>
2020-04-29 00:03:29 +0000

    Optimization 3

--- service-worker.js
+++ service-worker.js
@@ -51,14 +51,16 @@ self.addEventListener('fetch', (event) => {
 				}
 
 				return caches.open(RUNTIME).then((cache) => {
-					return fetch(event.request).then((response) => {
-						// Put a copy of the response in the runtime cache.
-						return cache
-							.put(event.request, response.clone())
-							.then(() => {
-								return response
-							})
-					})
+					return fetch(event.request)
+						.then((response) => {
+							// Put a copy of the response in the runtime cache.
+							return cache
+								.put(event.request, response.clone())
+								.then(() => {
+									return response
+								})
+						})
+						.catch((err) => console.error(err))
 				})
 			})
 		)
